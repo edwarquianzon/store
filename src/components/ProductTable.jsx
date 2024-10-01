987f6774ca1b43be86a093f9a26997af7ee5eff9
Anthooony13 <quianzon.edward@gmail.com>
2024-10-01 19:39:40 +0800

    Added filter functionality

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -18,7 +18,9 @@ const ProductTable = ({ headers, products }) => {
         </Table.THead>
 
         <Table.TBody>
-          <ProductCategoryHeader text={headers[0]} />
+          {sportingGoods.length > 0 && (
+            <ProductCategoryHeader text={headers[0]} />
+          )}
 
           {sportingGoods.map((sportingGood) => (
             <ProductItem
@@ -28,7 +30,9 @@ const ProductTable = ({ headers, products }) => {
             />
           ))}
 
-          <ProductCategoryHeader text={headers[1]} />
+          {electronics.length > 0 && (
+            <ProductCategoryHeader text={headers[1]} />
+          )}
 
           {electronics.map(({ id, name, price, type }) => (
             <ProductItem key={`${type}-${id}`} name={name} price={price} />
